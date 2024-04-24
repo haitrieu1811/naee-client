7f0c1d92eb5f6ead1e52aa79beb9ab0079dd9d67
Tran Hai Trieu <haitrieu2524@gmail.com>
2024-04-24 18:48:28 +0700

    feat: add shadcnui to the project

---
+++ src/app/(shop)/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+const ShopLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div>
+      Shop layout
+      {children}
+    </div>
+  );
+};
+
+export default ShopLayout;
